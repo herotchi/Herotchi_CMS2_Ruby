87d279597a90ab11bfe38e8936a76cd226ea00d0
herotchi <hidenochion+github@gmail.com>
2025-07-31 15:35:40 +0900

    公開側ログイン画面デザイン適用

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,5 +8,14 @@ class User < ApplicationRecord
   validates :name,
     presence: true,
     length: { maximum: UserConstants::NAME_LENGTH_MAX, allow_blank: true },
-    uniqueness: { allow_blank: true }
+    uniqueness: { allow_blank: true },
+    unless: :devise_password_reset?
+  validates :user_policy,
+    acceptance: { on: :create},
+    unless: :devise_password_reset?
+
+
+    def devise_password_reset?
+      reset_password_token.present?
+    end
 end
