483f4b0adb5c5918cf66f7d609bec34c11740e57
herotchi <hidenochion+github@gmail.com>
2025-08-22 14:08:47 +0900

    管理側タグ一覧画面の実装

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -4,4 +4,12 @@ class Tag < ApplicationRecord
     presence: true,
     length: { maximum: TagConstants::NAME_LENGTH_MAX, allow_blank: true },
     uniqueness: { allow_blank: true }
+
+  def self.ransackable_attributes(auth_object = nil)
+     %w[name]
+  end
+
+  def self.ransackable_associations(auth_object = nil)
+     []
+  end
 end
