a6062e1df8b7cf7908dccfffc76eb742d2a2c3b4
herotchi <hidenochion+github@gmail.com>
2025-09-08 12:31:47 +0900

    公開側お知らせ詳細画面の実装

--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -1,8 +1,16 @@
 class NewsController < ApplicationController
+  before_action :set_news, only: %i[ show ]
+
   def index
     @news = News.released.page(params[:page]).per(NewsConstants::PAGENATE_LIST_LIMIT)
   end
 
   def show
   end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_news
+      @news = News.find(params.expect(:id))
+    end
 end
