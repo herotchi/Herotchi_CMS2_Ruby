efd81dafd3f78bc69f86893df7be708600326ef7
herotchi <hidenochion+github@gmail.com>
2025-09-09 16:07:12 +0900

    公開側製品情報詳細画面の実装

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,4 +1,6 @@
 class ProductsController < ApplicationController
+  before_action :set_product, only: %i[ show ]
+
   def index
     @second_categories = SecondCategory.all
     @products = Product.search(params).order(id: :desc).page(params[:page]).per(ProductConstants::PAGENATE_LIST_LIMIT)
@@ -6,4 +8,10 @@ class ProductsController < ApplicationController
 
   def show
   end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_product
+      @product = Product.find(params.expect(:id))
+    end
 end
